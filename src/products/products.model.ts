cfe8c80c7923d6790473f9eb3845e8e8d0bc3f7a
dahtrgxnl <thelandisarnolds@gmail.com>
2025-05-16 12:29:32 -0400

    Started MOngoose

--- src/products/products.model.ts
+++ src/products/products.model.ts
@@ -1,5 +1,11 @@
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true}
+});
 export class Product{
-    
     constructor(public id: string, public title: string, public desc: string, public price: number){
         
     };
